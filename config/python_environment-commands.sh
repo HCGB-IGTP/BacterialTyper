97f652c2aea8a39ce8b1f8faf86a9c48c61d0028
jfsanchez <jfbioinformatics@gmail.com>
2019-04-08 12:16:42 +0200

    Add python requirements

---
+++ config/python_environment-commands.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=""
+python3 -m venv config/BacterialGenotyping_env
+source config/BacterialGenotyping_env/bin/activate
+
+## upgrade pip
+pip install -I --isolated --upgrade pip
+
+## install from requirements.txt file
+pip install -r ./config/python_requirements.txt
+
+
+##################################################
+## Modules
+##################################################
+## pip install -I --isolated fastqcparser
+## pip install -I --isolated configparser
+## pip install -I --isolated numpy
+## pip install -I --isolated scipy
+## pip install -I --isolated cython
+## pip install -I --isolated biopython
+## pip install -I --isolated pandas
+## pip install -I --isolated xlrd
+## pip install -I --isolated xlwt
+## pip install -I --isolated wget
+## pip install -I --isolated xtract
+## pip install -I --isolated ncbi-genome-download
+## pip install -I --isolated matplotlib
+## pip install -I --isolated multiqc
+##
+## pip freeze > requirements.txt
+##################################################
+
