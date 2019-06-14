58720239790b076e61b348f8f706c7c48191905a
jfsanchez <jfbioinformatics@gmail.com>
2019-06-14 13:44:14 +0200

    Update setting pip package with submodule

--- setup.py
+++ setup.py
@@ -4,7 +4,16 @@ import sys
 import glob
 from setuptools import setup, find_packages, Extension
 
+
+PhiSpy_files = [os.path.join('third_party', 'PhiSpy', x) for x in PhiSpy_files]
+PhiSpy_mod = Extension(
+    "PhiSpy",
+    PhiSpy_files,
+    include_dirs=[os.path.join('third_party', 'PhiSpy')],
+)
+
 setup(
+	ext_modules=[PhiSpy_mod],
     name='xxxx',
     version='xx.xx.xx',
     description='xxxxx',
@@ -14,7 +23,7 @@ setup(
     url='xxxx',
     scripts=glob.glob('scripts/*'),
     install_requires=[
-
+		## add additional modules
         'BeautifulSoup4 >= 4.1.0', 
         'biopython',
         'dendropy >= 4.2.0',
@@ -23,8 +32,7 @@ setup(
         'pysam >= 0.9.1',
         'pymummer<=0.10.3',
     ],
-    license='GPLv3',
-    
+    license='GPLv3',    
     classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
@@ -35,3 +43,4 @@ setup(
 
 ## https://github.com/sanger-pathogens/ariba/blob/master/setup.py
 ## https://dzone.com/articles/executable-package-pip-install
+## https://github.com/BillMills/pythonPackageLesson
