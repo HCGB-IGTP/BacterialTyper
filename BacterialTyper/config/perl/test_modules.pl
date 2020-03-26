8538c5a2349e70339b6f4fccb2fe62ce63c82fd3
JFsanchez <jfbioinformatics@gmail.com>
2020-03-26 16:11:45 +0100

    add perl config details

---
+++ BacterialTyper/config/perl/test_modules.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+use List::Util;
+use Getopt::Long;
+use Data::Dumper;
+use File::Copy;
+use File::Basename;
+use File::Spec;
+use File::Path;
+use Cwd;
+use File::Temp;
+use FindBin;
+use Log::Log4perl;
+  
