f47788a725e011b7bc29e43818c3286225b426c6
JFsanchez <jfbioinformatics@gmail.com>
2020-03-26 17:08:37 +0100

    add version

--- BacterialTyper/__init__.py
+++ BacterialTyper/__init__.py
@@ -1,8 +1,12 @@
-#from pkg_resources import get_distribution
-#try:
-#    __version__ = get_distribution('BacterialTyper').version
-#except:
-#    __version__ = 'local'
+from pkg_resources import get_distribution
+try:
+	__version__ = get_distribution('BacterialTyper').version
+except:
+	try:
+		__version__ = pkg_resources.resorce_filename('BacterialTyper', VERSION)
+	except:
+		__version__ = 'local'
+
 ### to include when distribution available
 ## version will be retrieve from setup.py
 
