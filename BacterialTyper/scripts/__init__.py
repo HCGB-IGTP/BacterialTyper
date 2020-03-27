49b700a83686be53b161aa57db6d6298b68d5cdd
jfsanchezherrero <jfsanchezherrero@gmail.com>
2020-03-27 09:52:39 +0100

    remove version from scripts folder

--- BacterialTyper/scripts/__init__.py
+++ BacterialTyper/scripts/__init__.py
@@ -1,12 +1,3 @@
-#from pkg_resources import get_distribution
-#try:
-#    __version__ = get_distribution('BacterialTyper').version
-#except:
-#    __version__ = 'local'
-### to include when distribution available
-## version will be retrieve from setup.py
-
-
 __all__ = [
     'annotation',
     'ariba_caller',
