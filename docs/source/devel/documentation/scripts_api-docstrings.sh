d2dc3ec8d69eda8475f5e1d01102875a2a4ad530
jfsanchezherrero <jfsanchezherrero@gmail.com>
2020-04-06 15:03:22 +0200

    update to include image pyan graph

--- docs/source/devel/documentation/scripts_api-docstrings.sh
+++ docs/source/devel/documentation/scripts_api-docstrings.sh
@@ -8,6 +8,12 @@ for i in `dir ../../../../BacterialTyper/`; do
 	echo "" >> $file; 
 	echo $name >> $file; 
 	echo "========" >> $file; 
+	echo "This script contains several functions. Here we show a graph" >> $file; 
+	echo "representation of the different functions and relationships among them:" >> $file;
+
+	echo ".. image:: ../../images/scripts_graph/"$name".png" >> $file;
+		echo ":align: center" >> $file;
+
 	echo ".. automodule:: BacterialTyper.scripts."$i >> $file; 
 	echo "    :members:" >> $file; 
 done;
