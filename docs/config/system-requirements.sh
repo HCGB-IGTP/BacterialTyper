90595b838658c7d7120dc19f78e86d46c4665e68
jfsanchez <jfsanchez@debian.jf>
2020-05-05 11:15:31 +0200

    add debian system packages

---
+++ docs/config/system-requirements.sh
@@ -0,0 +1,3 @@
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+sudo apt install graphviz
---
+++ env/apt-install.sh
@@ -0,0 +1,10 @@
+## Ubuntu/Debian/Mint
+sudo apt install git
+sudo apt install libz-dev
+sudo apt install build-essential
+sudo apt install python3 python3-dev python-dev python3-venv python-pip
+sudo apt install libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt install default-jre
+sudo apt install libdatetime-perl libxml-simple-perl 
+sudo apt install bioperl
+sudo cpan Bio::Perl
