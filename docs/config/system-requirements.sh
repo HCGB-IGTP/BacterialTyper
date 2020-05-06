f31f80679e603ecd8d36ce25453edb8544b76e2f
jfsanchez <jfsanchez@debian.jf>
2020-05-06 16:06:57 +0200

    update apt-install requirements

--- docs/config/system-requirements.sh
+++ docs/config/system-requirements.sh
@@ -1,3 +1,3 @@
-sudo apt-get install texlive-full
-sudo apt-get install texmaker
+sudo apt install texlive-full
+sudo apt install texmaker
 sudo apt install graphviz
--- env/apt-install.sh
+++ env/apt-install.sh
@@ -1,10 +1,12 @@
 ## Ubuntu/Debian/Mint
-sudo apt install git
-sudo apt install libz-dev
-sudo apt install build-essential
-sudo apt install python3 python3-dev python-dev python3-venv python-pip
-sudo apt install libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
-sudo apt install default-jre
-sudo apt install libdatetime-perl libxml-simple-perl 
-sudo apt install bioperl
+sudo apt install -y git
+sudo apt install -y libz-dev
+sudo apt install -y build-essential
+sudo apt install -y cmake
+sudo apt install -y python3 python3-dev python-dev python3-venv python-pip
+sudo apt install -y libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt install -y default-jre
+sudo apt install -y libdatetime-perl libxml-simple-perl 
+sudo apt install -y bioperl
 sudo cpan Bio::Perl
+ 
