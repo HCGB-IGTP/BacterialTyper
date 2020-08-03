b048d20c0110c8e5f75e9f0da6000f6fcd77c6df
jfsanchezherrero <jfbioinformatics@gmail.com>
2020-08-03 15:45:51 +0200

    update

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ def get_require_modules():
     """
     Get main python requirements modules
     """
-    with open("./docs/source/data/python_requirements_summary.csv", 'r') as f:
-        myModules = [line.strip() for line in f]
+    with open("./docs/source/data/installation/python_requirements_summary.csv", 'r') as f:
+        myModules = [line.strip().split(',')[0] for line in f]
     
     ## To Do: Debug
     print(myModules)
@@ -25,29 +25,39 @@ def get_version():
     return v
 
 ### subdmodules:: Islandpath-DIMOB
-islandpath_files = [os.path.join('third_party', 'Islandpath-DIMOB', x) for x in islandpath_files]
-islandpath_mod = Extension(
-    "Islandpath-DIMOB",
-    islandpath_files,
-    include_dirs=[os.path.join('third_party', 'Islandpath-DIMOB')],
-)
+#islandpath_files = [os.path.join('third_party', 'Islandpath-DIMOB', x) for x in islandpath_files]
+#islandpath_mod = Extension(
+#    "Islandpath-DIMOB",
+#    islandpath_files,
+#    include_dirs=[os.path.join('third_party', 'Islandpath-DIMOB')],
+#)
+
+long_description_text = ""
+with open("README.md", "r") as fh:
+    long_description_text = fh.read()
 
 setup(
-	name='BacterialTyper',
+    name='BacterialTyper',
     version=get_version(),
-    description='xxxxx',
-    packages = find_packages(),
-    author='Jose F Sanchez Herrero',
-    author_email='xxxx',
-    url='xxxx',
-    scripts=["main/BacterialTyper.py"],
+
+    scripts=glob.glob('main/*'),
+    author="Jose F. Sanchez-Herrero",
+    author_email="jfbioinformatics@gmail.com",
+
+    long_description_content_type="text/markdown",
+    long_description=long_description_text,
+
+    url="https://github.com/HCGB-IGTP/BacterialTyper/",
+
     install_requires=get_require_modules(),
-    license='GPLv3',    
+    packages = find_packages(),
+
+    license='MIT License',    
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
 
